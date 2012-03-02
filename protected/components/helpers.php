d806419fd15aca9d10c7be0232bde529ee028862
Hesedel <hes@pajaroncreative.com>
2012-03-02 06:42:29 -0500

    update timeago

--- protected/components/helpers.php
+++ protected/components/helpers.php
@@ -3,14 +3,14 @@
 function time_local($time, $options = array())
 {
 	$defaults = array(
-		'timezone'=>Yii::app()->params['timeZone'],
+		'timeZone'=>Yii::app()->params['timeZone'],
 		'format'=>false,
 		'offset'=>0,
 	);
 	$options = array_merge($defaults, $options);
 
 	$serverTime = new DateTime(date('Y-m-d H:i:s'), new DateTimeZone(date_default_timezone_get()));
-	$targetTime = new DateTime(date('Y-m-d H:i:s'), new DateTimeZone($options['timezone']));
+	$targetTime = new DateTime(date('Y-m-d H:i:s'), new DateTimeZone($options['timeZone']));
 	$timeZoneOffset = $targetTime->getOffset() - $serverTime->getOffset();
 	$dateTime = new DateTime($time);
 	$unix = $dateTime->format('U') + $timeZoneOffset + $options['offset'];
