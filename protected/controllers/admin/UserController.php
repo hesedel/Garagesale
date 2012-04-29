ccae07cd708b281a411d2c0fa02a63c3fa6690ed
Hesedel <hes@pajaroncreative.com>
2012-04-30 04:54:33 +0800

    ui updates

--- protected/controllers/admin/UserController.php
+++ protected/controllers/admin/UserController.php
@@ -40,7 +40,7 @@ class UserController extends Controller
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+				'users'=>array('admin','super'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
