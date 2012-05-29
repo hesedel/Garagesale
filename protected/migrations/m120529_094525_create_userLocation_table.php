b5cf14a80659b78c2f974e9e50049f4531d95d70
Hesedel <hes@pajaroncreative.com>
2012-05-30 07:49:06 +0800

    fix again

--- protected/migrations/m120529_094525_create_userLocation_table.php
+++ protected/migrations/m120529_094525_create_userLocation_table.php
@@ -7,9 +7,9 @@ class m120529_094525_create_userLocation_table extends CDbMigration
 		$this->createTable('user_location', array(
 			'id'=>'mediumint UNSIGNED PRIMARY KEY',
 			'name'=>'varchar(32) NOT NULL',
-		));
+		), 'ENGINE InnoDB');
 		$this->addColumn('user', 'location_id', 'mediumint UNSIGNED');
-		$this->createIndex('location_id', 'user', 'location_id');
+		//$this->createIndex('location_id', 'user', 'location_id');
 		$this->addForeignKey('location', 'user', 'location_id', 'user_location', 'id', 'SET NULL', 'CASCADE');
 		$this->insert('user_location', array(
 			'id'=>8000,
