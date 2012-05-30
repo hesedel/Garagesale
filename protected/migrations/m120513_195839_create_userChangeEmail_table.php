2817f77730920deab344137b6cbe02dcde583fdd
Hesedel <hes@pajaroncreative.com>
2012-05-30 09:10:38 +0800

    revert last

--- protected/migrations/m120513_195839_create_userChangeEmail_table.php
+++ protected/migrations/m120513_195839_create_userChangeEmail_table.php
@@ -8,7 +8,7 @@ class m120513_195839_create_userChangeEmail_table extends CDbMigration
 			'id'=>'varchar(32) PRIMARY KEY',
 			'user_id'=>'varchar(64) NOT NULL UNIQUE',
 			'email'=>'varchar(64) NOT NULL UNIQUE',
-		), 'ENGINE InnoDB');
+		));
 		$this->addForeignKey('changeEmail', 'user_changeEmail', 'user_id', 'user', 'id', 'CASCADE', 'CASCADE');
 	}
 
