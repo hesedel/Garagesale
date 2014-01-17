b0fc7b1fc272ef7b3814055565760b343bafe594
Hesedel <hes@pajaroncreative.com>
2014-01-17 18:42:43 +1100

    fixed error

--- themes/responsive/views/layouts/main.php
+++ themes/responsive/views/layouts/main.php
@@ -1,43 +1,49 @@
-<?php $this->beginContent('//layouts/main') ?>
+<?php if(env_is(array('sta', 'pro'))): ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+<?php else: ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php endif ?>
 
-<div id="column2">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"<?php echo preg_match('/^localhost./', $_SERVER['HTTP_HOST']) == 0 ? ' xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml"' : '' ?>>
 
-<div id="content-container">
+<?php $this->renderPartial('/layouts/_head') ?>
 
-	<div id="content">
+<!--[if lt IE 7]><body class="no-js ie6"><![endif]-->
+<!--[if IE 7]><body class="no-js ie7"><![endif]-->
+<!--[if IE 8]><body class="no-js ie8"><![endif]-->
+<!--[if IE 9]><body class="no-js ie9"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><body class="no-js"><!--<![endif]-->
 
-		<?php if(isset($this->breadcrumbs)) {
-			$this->widget('zii.widgets.CBreadcrumbs', array(
-				'links' => $this->breadcrumbs,
-				'separator' => ' &#160; / &#160; ',
-			));
-		} ?>
+<div id="body">
+
+	<div id="body-top"></div>
+
+	<div id="page">
+
+		<!--[if (gt IE 7)|!(IE)]><!--><?php $this->renderPartial('/layouts/_table') ?><!--<![endif]-->
+		<!--[if lt IE 8]><?php $this->renderPartial('/layouts/_table', array('legacy' => true)) ?><![endif]-->
+
+		<?php if(Yii::app()->user->hasFlash('success')): ?>
+		<div id="alert" class="alert alert-success timeout"><?php echo Yii::app()->user->getFlash('success') ?></div>
+		<?php endif ?>
 
 		<?php echo $content ?>
 
-	</div><!-- #content -->
+	</div><!-- #page -->
 
-</div><!-- #content-container -->
+</div><!-- #body -->
 
-<div id="sidebar">
+<div id="footer"></div><!-- #footer -->
 
-	<?php echo $this->clips['sidebar'] ?>
+</body>
 
-	<?php $this->beginWidget('zii.widgets.CPortlet', array(
-			'title' => 'Operations',
-	)) ?>
+</html>
 
-	<?php $this->widget('zii.widgets.CMenu', array(
-		'items' => $this->menu,
-		'htmlOptions' => array('class' => 'operations'),
-	)) ?>
+<?php
+Yii::app()->clientScript->registerScript('form',
+	file_get_contents('js/form.js'),
+CClientScript::POS_READY);
 
-	<?php $this->endWidget() ?>
-
-</div><!-- #sidebar -->
-
-<div class="clear"></div>
-
-</div><!-- #column2 -->
-
-<?php $this->endContent() ?>
+Yii::app()->clientScript->registerScript('main',
+	file_get_contents('js/main.js'),
+CClientScript::POS_READY);
