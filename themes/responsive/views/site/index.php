71b912a80a0af7a37fcc02bd7c7caebb305114e8
Hesedel <hes@pajaroncreative.com>
2014-03-10 23:24:56 +1100

    header/homepage refinements

--- themes/responsive/views/site/index.php
+++ themes/responsive/views/site/index.php
@@ -5,20 +5,28 @@ $this->layout = 'column1';
 
 <div id="site_index">
 
-<h2>Featured Items</h2>
-
-<?php $this->renderPartial('/item/_index', array(
-	'dataProvider' => $dataProvider_featured,
-	'options' => array(
-		'toolbox' => array(
-			'viewButton' => false,
-			'sortButton' => false,
-		),
-	),
-)); ?>
-
 <div class="row">
-	<div class="col-md-8">
+	<div class="col-md-12 col-lg-9">
+
+		<h2>Featured Items</h2>
+
+		<?php $this->renderPartial('/item/_index', array(
+			'dataProvider' => $dataProvider_featured,
+			'options' => array(
+				'toolbox' => array(
+					'viewButton' => false,
+					'sortButton' => false,
+				),
+			),
+		)); ?>
+
+	</div>
+	<div class="col-md-4 col-md-push-8 col-lg-3 col-lg-push-0">
+
+		...
+
+	</div>
+	<div class="col-md-8 col-md-pull-4 col-lg-9 col-lg-pull-0">
 
 			<h3>Latest Items</h3>
 
