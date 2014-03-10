f06f5873fdbae04fe568bfa09c54310fb57a4f3b
Hesedel <hes@pajaroncreative.com>
2014-03-10 23:30:36 +1100

    homepage refinements

--- themes/responsive/views/site/index.php
+++ themes/responsive/views/site/index.php
@@ -6,7 +6,7 @@ $this->layout = 'column1';
 <div id="site_index">
 
 <div class="row">
-	<div class="col-md-12 col-lg-9">
+	<div class="col-md-12 col-lg-8">
 
 		<h2>Featured Items</h2>
 
@@ -21,12 +21,12 @@ $this->layout = 'column1';
 		)); ?>
 
 	</div>
-	<div class="col-md-4 col-md-push-8 col-lg-3 col-lg-push-0">
+	<div class="col-md-4 col-md-push-8 col-lg-4 col-lg-push-0">
 
 		...
 
 	</div>
-	<div class="col-md-8 col-md-pull-4 col-lg-9 col-lg-pull-0">
+	<div class="col-md-8 col-md-pull-4 col-lg-8 col-lg-pull-0">
 
 			<h3>Latest Items</h3>
 
