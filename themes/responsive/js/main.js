fb86d84670987469d224b2efc52b5338580f69cc
Hesedel <hes@pajaroncreative.com>
2014-03-11 14:48:44 +1100

    header filter button refinement

--- themes/responsive/js/main.js
+++ themes/responsive/js/main.js
@@ -1,49 +1,56 @@
 $(function() {
 
-$('.a', '#user').data('hover', false);
-$('.a > a', '#user').bind( {
-	touchstart: function() {
-		if(!$(this).parent().data('hover')) {
-			$(this).parent().data('hover', true);
-			$('.dropdown-menu', $(this).parent()).removeClass('hidden');
-		} else {
-			$(this).parent().data('hover', false);
-			$('.dropdown-menu', $(this).parent()).addClass('hidden');
+	$('.a', '#user').data('hover', false);
+	$('.a > a', '#user').bind( {
+		touchstart: function() {
+			if(!$(this).parent().data('hover')) {
+				$(this).parent().data('hover', true);
+				$('.dropdown-menu', $(this).parent()).removeClass('hidden');
+			} else {
+				$(this).parent().data('hover', false);
+				$('.dropdown-menu', $(this).parent()).addClass('hidden');
+				return false;
+			}
+		},
+		touchend: function() {
 			return false;
 		}
-	},
-	touchend: function() {
-		return false;
-	}
-});
+	});
 
-$('.a', '#filters').bind('click', function() {
-	if(!$(this).hasClass('is-clicked')) {
-		$(this).addClass('is-clicked');
-		$('.filters', '#table').removeClass('is-hidden');
-	} else {
-		$(this).removeClass('is-clicked');
-		$('.filters', '#table').addClass('is-hidden');
-	}
-});
+	$('.a', '#filters').bind( {
+		mouseover: function() {
+			$(this).removeClass('is-inactive');
+		},
+		click: function() {
+			if(!$(this).hasClass('is-active')) {
+				$(this).addClass('is-active');
+				$('.filters', '#table').removeClass('is-hidden');
+			} else {
+				$(this)
+					.removeClass('is-active')
+					.addClass('is-inactive');
+				$('.filters', '#table').addClass('is-hidden');
+			}
+		}
+	});
 
-$('input[type=text]', '#search').bind( {
-	focus: function() {
-		$('.a:not(.is-clicked)', '#filters').trigger('click');
-	},
-	blur: function() {
-		$('.a.is-clicked', '#filters').trigger('click');
-	}
-});
+	$('input[type=text]', '#search').bind( {
+		focus: function() {
+			$('.a:not(.is-active)', '#filters').trigger('click');
+		},
+		blur: function() {
+			$('.a.is-active', '#filters').trigger('click');
+		}
+	});
 
-$('time.timeago').timeago();
+	$('time.timeago').timeago();
 
 });
 
 $(window).load(function() {
 
-setTimeout(function() {
-	$('.alert-success.timeout').slideUp();
-}, 10000);
+	setTimeout(function() {
+		$('.alert-success.timeout').slideUp();
+	}, 10000);
 
 });
