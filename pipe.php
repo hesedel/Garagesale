9ed8c60b2513d8fc6a04156f4ef83d2df2ab7609
janzenz <jz@email.fake>
2014-04-05 13:45:34 +0800

    Added email template.

--- pipe.php
+++ pipe.php
@@ -3,6 +3,7 @@
 //include email parser  
 require_once('pipe/rfc822_addresses.php');  
 require_once('pipe/mime_parser.php'); 
+require_once('protected/components/CSSToInlineStyles.php');
 
 // read email in from stdin  
 $fd = fopen("php://stdin", "r");  
@@ -58,11 +59,35 @@ if(substr($decoded[0]['Headers']['content-type:'],0,strlen('text/plain')) == 'te
       
     $body = $decoded[0]['Parts'][0]['Parts'][0]['Body'];  
   
-}  
+}
   
 //print out our data  
 $message = "<br><br>Message ID: $messageID<br><br>Reply ID: $replyToID<br><br>Subject: $subject<br><br>To: $toName $toEmail<br><br>From: $fromName $fromEmail<br><br>Body: $body<br><br>";  
-  
+
+/*
+- Send out template on first message.
+1. Convert HEX to ID
+2. Then retrieve data from "item_contact" table using the converted ID. 
+*/
+
+ob_start();
+
+/*
+Get these datas and send it to the file:
+$name
+$link
+$team name
+*/
+$name = 'Janzen';
+$link = 'http://local.garagsale.com';
+$team = 'Garagesale';
+include('pipe/_emailTemplate.php');
+$template = ob_get_clean();
+$body=new CSSToInlineStyles(
+	$template,file_get_contents('css/emailWrapper.css')
+);
+
+
 //show all the decoded email info  
 // print_r($decoded); 
-mail('janzen.contact@gmail.com', 'Test Message', $message);
+mail('janzen.contact@gmail.com', $subject, $body);
