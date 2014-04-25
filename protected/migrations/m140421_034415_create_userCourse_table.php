10f5212cee04a7d04cfed8d8396f323872b7b85c
marshamatilda <mmwhite83@gmail.com>
2014-04-25 13:15:50 +1000

    #29
    Added UTS Course Areas needed for Profile Creation

--- protected/migrations/m140421_034415_create_userCourse_table.php
+++ protected/migrations/m140421_034415_create_userCourse_table.php
@@ -13,13 +13,54 @@ class m140421_034415_create_userCourse_table extends CDbMigration
 		$this->addForeignKey('course_area', 'user', 'course_area_id', 'user_course', 'id', 'CASCADE', 'SET NULL');
 		$this->insert('user_course', array(
 			'id'=>0,
-			'name'=>'Law',
+			'name'=>'Business',
 		));
 
 		$this->insert('user_course', array(
 			'id'=>1,
+			'name'=>'Communication',
+		));
+		$this->insert('user_course', array(
+			'id'=>2,
+			'name'=>'Creative Intelligence and Innovation',
+		));
+		$this->insert('user_course', array(
+			'id'=>3,
+			'name'=>'Design, Architecture and Building',
+		));
+		$this->insert('user_course', array(
+			'id'=>4,
+			'name'=>'Education',
+		));
+		$this->insert('user_course', array(
+			'id'=>5,
+			'name'=>'Engineering',
+		));
+		$this->insert('user_course', array(
+			'id'=>6,
+			'name'=>'Health',
+		));
+		$this->insert('user_course', array(
+			'id'=>7,
+			'name'=>'Information Technology',
+		));
+		$this->insert('user_course', array(
+			'id'=>8,
+			'name'=>'International Studies',
+		));
+		$this->insert('user_course', array(
+			'id'=>9,
+			'name'=>'Law',
+		));
+		$this->insert('user_course', array(
+			'id'=>10,
+			'name'=>'Pharmacy',
+		));
+		$this->insert('user_course', array(
+			'id'=>11,
 			'name'=>'Science',
 		));
+
 	}
 
 	public function safeDown()
