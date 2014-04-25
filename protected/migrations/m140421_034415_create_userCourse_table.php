778209a03b21d9f4162151886206fce0a6824059
Hesedel <hes@pajaroncreative.com>
2014-04-25 16:43:11 +1000

    #29 corrections

--- protected/migrations/m140421_034415_create_userCourse_table.php
+++ protected/migrations/m140421_034415_create_userCourse_table.php
@@ -6,16 +6,15 @@ class m140421_034415_create_userCourse_table extends CDbMigration
 	{
 		$this->createTable('user_course', array(
 			'id'=>'mediumint UNSIGNED PRIMARY KEY',
-			'name'=>'varchar(32) NOT NULL',
+			'name'=>'varchar(64) NOT NULL',
 		), 'ENGINE InnoDB');
-		$this->addColumn('user', 'course_area_id', 'mediumint UNSIGNED');
-		//$this->createIndex('location_id', 'user', 'location_id');
-		$this->addForeignKey('course_area', 'user', 'course_area_id', 'user_course', 'id', 'CASCADE', 'SET NULL');
+		$this->addColumn('user', 'course_id', 'mediumint UNSIGNED');
+		$this->addForeignKey('course', 'user', 'course_id', 'user_course', 'id', 'SET NULL', 'CASCADE');
+
 		$this->insert('user_course', array(
 			'id'=>0,
 			'name'=>'Business',
 		));
-
 		$this->insert('user_course', array(
 			'id'=>1,
 			'name'=>'Communication',
@@ -65,8 +64,8 @@ class m140421_034415_create_userCourse_table extends CDbMigration
 
 	public function safeDown()
 	{
-		$this->dropForeignKey('course_area', 'user');
-		$this->dropColumn('user', 'course_area_id');
+		$this->dropForeignKey('course', 'user');
+		$this->dropColumn('user', 'course_id');
 		$this->dropTable('user_course');
 	}
 }
