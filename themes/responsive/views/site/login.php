50527df06805fcb5f7e48620b19bd2818a6acdbd
Hesedel <hes@pajaroncreative.com>
2014-04-25 21:11:10 +1000

    updates

--- themes/responsive/views/site/login.php
+++ themes/responsive/views/site/login.php
@@ -1,6 +1,6 @@
 <?php
-$this->pageTitle=Yii::app()->name . ' - Login';
-$this->breadcrumbs=array(
+$this->pageTitle = Yii::app()->name . ' - Login';
+$this->breadcrumbs = array(
 	'Login',
 );
 
@@ -10,41 +10,41 @@ $this->layout = '/layouts/transition';
 <div id="site_login">
 	<h2>Login</h2>
 	<div class="form">
-		<?php $form=$this->beginWidget('CActiveForm', array(
-			'id'=>'login-form',
-			'enableClientValidation'=>true,
+		<?php $form = $this->beginWidget('CActiveForm', array(
+			'id' => 'login-form',
+			'enableClientValidation' => true,
 			/*
-			'clientOptions'=>array(
-				'validateOnSubmit'=>true,
+			'clientOptions' => array(
+				'validateOnSubmit' => true,
 			),
 			*/
-			'focus'=>strlen($model->username) == 0 || $model->getErrorCode() == 1 ? array($model,'username') : array($model,'password'),
+			'focus' => strlen($model->username) == 0 || $model->getErrorCode() == 1 ? array($model, 'username') : array($model, 'password'),
 		)); ?>
 		<table class="form" summary="Login">
 			<caption class="hide">Login</caption>
 			<tbody>
 
 				<tr>
-					<th><?php echo $form->labelEx($model,'username'); ?></th>
+					<th><?php echo $form->labelEx($model, 'username'); ?></th>
 					<td>
 						<div class="input-text">
-							<?php echo $form->textField($model,'username'); ?>
+							<?php echo $form->textField($model, 'username'); ?>
 							<span class="placeholder">Your username or email address</span>
 						</div>
-						<?php echo $form->error($model,'username'); ?>
+						<?php echo $form->error($model, 'username'); ?>
 					</td>
 				</tr>
 
 				<tr>
-					<th><?php echo $form->labelEx($model,'password'); ?></th>
+					<th><?php echo $form->labelEx($model, 'password'); ?></th>
 					<td>
 						<div class="input-text">
-							<?php echo $form->passwordField($model,'password'); ?>
+							<?php echo $form->passwordField($model, 'password'); ?>
 							<span class="placeholder">Your password</span>
 						</div>
-						<?php echo $form->error($model,'password'); ?>
+						<?php echo $form->error($model, 'password'); ?>
 						<?php if($model->getErrorCode() == 1 || $model->getErrorCode() == 2):
-							echo CHtml::link('Forgot password?', array('/admin/user/password_forgot', 'id'=>$model->username), array('class'=>'forgotPassword'));
+							echo CHtml::link('Forgot password?', array('/admin/user/password_forgot', 'id' => $model->username), array('class' => 'forgotPassword'));
 						endif; ?>
 					</td>
 				</tr>
@@ -55,11 +55,11 @@ $this->layout = '/layouts/transition';
 				<tr>
 					<?php //<th>&#160;</th> ?>
 					<td colspan="2">
-						<?php echo $form->checkBox($model,'rememberMe'); ?>
-						<?php echo $form->label($model,'rememberMe'); ?>
-						<?php echo CHtml::link('Cancel', Yii::app()->user->getReturnUrl(), array('class'=>'g-button')); ?>
-						<?php echo CHtml::linkButton('Login', array('class'=>'submit g-button orange')); ?>
-						<?php echo CHtml::submitButton('Login', array('class'=>'submit g-button orange')); ?>
+						<?php echo $form->checkBox($model, 'rememberMe'); ?>
+						<?php echo $form->label($model, 'rememberMe'); ?>
+						<?php echo CHtml::link('Cancel', Yii::app()->user->getReturnUrl(), array('class' => 'g-button')); ?>
+						<?php echo CHtml::linkButton('Login', array('class' => 'submit g-button orange')); ?>
+						<?php echo CHtml::submitButton('Login', array('class' => 'submit g-button orange')); ?>
 					</td>
 				</tr>
 
--- themes/responsive/views/site/register.php
+++ themes/responsive/views/site/register.php
@@ -1,24 +1,24 @@
 <?php
-$this->pageTitle=Yii::app()->name . ' - Register';
-$this->breadcrumbs=array(
+$this->pageTitle = Yii::app()->name . ' - Register';
+$this->breadcrumbs = array(
 	'Register',
 );
 
-$this->layout='column2';
+$this->layout = 'column2';
 ?>
 
 <div class="g-form" id="site_register">
 	<h2>Register</h2>
 	<div class="form">
-		<?php $form=$this->beginWidget('CActiveForm', array(
-			'id'=>'register-form',
-			'enableClientValidation'=>true,
+		<?php $form = $this->beginWidget('CActiveForm', array(
+			'id' => 'register-form',
+			'enableClientValidation' => true,
 			/*
-			'clientOptions'=>array(
-				'validateOnSubmit'=>true,
+			'clientOptions' => array(
+				'validateOnSubmit' => true,
 			),
 			*/
-			'focus'=>array($model,'id'),
+			'focus' => array($model, 'id'),
 		)); ?>
 		<?php //echo $form->errorSummary($model); ?>
 		<table class="form" summary="Register">
@@ -26,111 +26,111 @@ $this->layout='column2';
 			<tbody>
 
 				<tr>
-					<th><?php echo $form->labelEx($model,'id'); ?></th>
+					<th><?php echo $form->labelEx($model, 'id'); ?></th>
 					<td>
 						<div class="input-text">
-							<?php echo $form->textField($model,'id'); ?>
+							<?php echo $form->textField($model, 'id'); ?>
 							<span class="placeholder">Only small letters, numbers, and underscores are allowed.</span>
 						</div>
-						<?php echo $form->error($model,'id'); ?>
+						<?php echo $form->error($model, 'id'); ?>
 					</td>
 				</tr>
 
 				<tr>
-					<th><?php echo $form->labelEx($model,'email'); ?></th>
+					<th><?php echo $form->labelEx($model, 'email'); ?></th>
 					<td>
 						<div class="input-text">
-							<?php echo $form->textField($model,'email'); ?>
+							<?php echo $form->textField($model, 'email'); ?>
 							<span class="placeholder">Your email address</span>
 						</div>
-						<?php echo $form->error($model,'email'); ?>
+						<?php echo $form->error($model, 'email'); ?>
 					</td>
 				</tr>
 
 				<?php /*
 				<tr class="repeat">
-					<th><?php echo $form->labelEx($model,'email_repeat'); ?></th>
+					<th><?php echo $form->labelEx($model, 'email_repeat'); ?></th>
 					<td>
 						<div class="input-text">
-							<?php echo $form->textField($model,'email_repeat'); ?>
+							<?php echo $form->textField($model, 'email_repeat'); ?>
 							<span class="placeholder">Re-enter your email address</span>
 						</div>
-						<?php echo $form->error($model,'email_repeat'); ?>
+						<?php echo $form->error($model, 'email_repeat'); ?>
 					</td>
 				</tr>
 				*/ ?>
 
 				<tr>
-					<th><?php echo $form->labelEx($model,'password'); ?></th>
+					<th><?php echo $form->labelEx($model, 'password'); ?></th>
 					<td>
 						<div class="input-text">
-							<?php echo $form->passwordField($model,'password'); ?>
+							<?php echo $form->passwordField($model, 'password'); ?>
 							<span class="placeholder">Create a password.</span>
 						</div>
-						<?php echo $form->error($model,'password'); ?>
+						<?php echo $form->error($model, 'password'); ?>
 					</td>
 				</tr>
 
 				<?php /*
 				<tr class="repeat">
-					<th><?php echo $form->labelEx($model,'password_repeat'); ?></th>
+					<th><?php echo $form->labelEx($model, 'password_repeat'); ?></th>
 					<td>
 						<div class="input-text">
-							<?php echo $form->passwordField($model,'password_repeat'); ?>
+							<?php echo $form->passwordField($model, 'password_repeat'); ?>
 							<span class="placeholder">Re-enter the password you created.</span>
 						</div>
-						<?php echo $form->error($model,'password_repeat'); ?>
+						<?php echo $form->error($model, 'password_repeat'); ?>
 					</td>
 				</tr>
 				*/ ?>
 
 				<tr>
-					<th><?php echo $form->labelEx($model,'name_first'); ?></th>
+					<th><?php echo $form->labelEx($model, 'name_first'); ?></th>
 					<td>
 						<div class="input-text">
-							<?php echo $form->textField($model,'name_first'); ?>
+							<?php echo $form->textField($model, 'name_first'); ?>
 							<span class="placeholder">Your first name</span>
 						</div>
-						<?php echo $form->error($model,'name_first'); ?>
+						<?php echo $form->error($model, 'name_first'); ?>
 					</td>
 				</tr>
 
 				<?php /*
 				<tr>
-					<th><?php echo $form->labelEx($model,'name_last'); ?></th>
+					<th><?php echo $form->labelEx($model, 'name_last'); ?></th>
 					<td>
 						<div class="input-text">
-							<?php echo $form->textField($model,'name_last'); ?>
+							<?php echo $form->textField($model, 'name_last'); ?>
 							<span class="placeholder">Your last name</span>
 						</div>
-						<?php echo $form->error($model,'name_last'); ?>
+						<?php echo $form->error($model, 'name_last'); ?>
 					</td>
 				</tr>
 				*/ ?>
 
 				<?php /*
 				<tr>
-					<th><?php echo $form->labelEx($model,'phone'); ?></th>
+					<th><?php echo $form->labelEx($model, 'phone'); ?></th>
 					<td>
 						<div class="input-text">
-							<?php echo $form->textField($model,'phone'); ?>
+							<?php echo $form->textField($model, 'phone'); ?>
 							<span class="placeholder">The phone number to contact you with</span>
 						</div>
-						<?php echo $form->error($model,'phone'); ?>
+						<?php echo $form->error($model, 'phone'); ?>
 					</td>
 				</tr>
 				*/ ?>
 
 				<?php if(CCaptcha::checkRequirements()): ?>
 				<tr>
-					<th><?php echo $form->labelEx($model,'verifyCode'); ?></th>
+					<th><?php echo $form->labelEx($model, 'verifyCode'); ?></th>
 					<td>
-						<div class="captcha"><?php $this->widget('CCaptcha',array('showRefreshButton'=>false)); ?></div>
+						<div class="captcha"><?php $this->widget('CCaptcha', array('showRefreshButton' => false)); ?></div>
 						<div class="input-text">
-							<?php echo $form->textField($model,'verifyCode'); ?>
+							<?php echo $form->textField($model, 'verifyCode'); ?>
 							<span class="placeholder">Please enter the letters as they are shown in the image above.</span>
 						</div>
-						<?php echo $form->error($model,'verifyCode'); ?>
+						<?php echo $form->error($model, 'verifyCode'); ?>
 					</td>
 				</tr>
 				<?php endif; ?>
@@ -141,9 +141,9 @@ $this->layout='column2';
 				<tr>
 					<?php //<th>&#160;</th> ?>
 					<td colspan="2">
-						<?php echo CHtml::link('Cancel', Yii::app()->user->getReturnUrl(), array('class'=>'g-button')); ?>
-						<?php echo CHtml::linkButton('Register', array('class'=>'submit g-button orange')); ?>
-						<?php echo CHtml::submitButton('Register', array('class'=>'submit g-button orange')); ?>
+						<?php echo CHtml::link('Cancel', Yii::app()->user->getReturnUrl(), array('class' => 'g-button')); ?>
+						<?php echo CHtml::linkButton('Register', array('class' => 'submit g-button orange')); ?>
+						<?php echo CHtml::submitButton('Register', array('class' => 'submit g-button orange')); ?>
 					</td>
 				</tr>
 
