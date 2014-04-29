a6cfc50efe371b73106895446e4a7b747fc25484
Hesedel <hes@pajaroncreative.com>
2014-04-29 13:52:29 +1000

    small change

--- protected/views/item/_sendMessage-email.php
+++ protected/views/item/_sendMessage-email.php
@@ -1,12 +1,10 @@
-Dear <?php echo $name_poster ?>,
+Dear <?php echo $name_poster; ?>,
 
 
 You've received this email because you've just recently received a message
-from <?php echo $name_replier ?>.
+from <?php echo $name_replier; ?>.
 
-To activate your account, please click the following link:
-
-<?php echo $message ?>
+<?php echo $message; ?>
 
 Cheers,
-<?php echo Yii::app()->name ?> Team
+<?php echo Yii::app()->name; ?> Team
