ffa0076aa28089379c16bde16e2c94692be31671
AmyScott <amethyst.au@gmail.com>
2014-05-01 00:01:18 +1000

    [#70213982] FAQs Updated
    FAQs Updated - HTML applied

--- themes/responsive/views/site/pages/faqs.php
+++ themes/responsive/views/site/pages/faqs.php
@@ -1,1 +1,21 @@
-FAQs
+<h1>Frequently Asked Questions</h1>
+
+<h2>How do I upload an item to sell?</h2>
+<p>From the Stycle home page, select "Sell".</p>
+<p>Complete the form and list your item for sale.</p>
+
+<h2>How do I contact a buyer or seller?</h2>
+<p>There are two ways to contact a buyer or seller:</p>
+<ol>
+<li>Go to the profile page of the buyer or seller and message them</li>
+<li>If you are a buyer, go to the item you are interested in buying and message the seller</li>
+</ol>
+
+<h2>What is a pm?</h2>
+<p>The Prime Minister of course! But in the case of our site, a pm means a private message. Check your inbox if someone tells you they have sent you a pm.</p>
+
+<h2>How do I report an item?</h2>
+<p>Report an item online through Stycle's <a href="http://www."report an item form</a>.</p> 
+
+<h2>How do I report a user?</h2>
+<p>Report a user online through Stycle's <a href="http://www."report a user form</a>.</p> 
