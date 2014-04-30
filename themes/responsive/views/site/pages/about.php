0c0e758edd55f7517afd9ad4cc8733fad0041fc2
AmyScott <amethyst.au@gmail.com>
2014-05-01 00:19:51 +1000

    [#70214014] About content added
    About content added, html applied

--- themes/responsive/views/site/pages/about.php
+++ themes/responsive/views/site/pages/about.php
@@ -6,5 +6,12 @@ $this->breadcrumbs=array(
 ?>
 <h1>About</h1>
 
-<p>This is a "static" page. You may change the content of this page
-by updating the file <tt><?php echo __FILE__; ?></tt>.</p>
+<p>Stycle is a student community space to buy and sell second hand items. It is also a space for community shoutouts and for students to find out about events happening locally.</p>
+
+<p>The best thing about Stycle is it is free to use! Free to buy and sell! Free to put up a shout out! And you can do all this from your mobile!</p>
+
+<p>Stycle was founded in May 2014 by a team of University students, Adam, Amy, Hes and Marsha. Their goal was to create a local space for students to make some cash through selling items and also buy items they need for their course, but it evolved as they found out that students needs come in many shapes and forms.</p>
+
+<p>Since it's first sale, Stycle has become a student space to sell anything from course materials, to furniture, to material, to computers, to baby clothes, to last night's fancy dress costume! What will you find on the Stycle site?</p>
+
+<p><tt><?php echo __FILE__; ?></tt>.</p>
