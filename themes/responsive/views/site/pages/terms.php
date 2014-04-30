f2799d4d14b126bc4f226d8caa2abeaf15e21557
AmyScott <amethyst.au@gmail.com>
2014-04-30 23:36:34 +1000

    [#70140878] Terms content added
    done

--- themes/responsive/views/site/pages/terms.php
+++ themes/responsive/views/site/pages/terms.php
@@ -1,1 +1,79 @@
-Terms
+
+<h1>Terms</h1>
+<p>These Terms apply to all users of Stycle, including users who are not registered users. Please read the Terms carefully before using Stycle.</p>
+<h2>1. Access to Stycle</h2>
+<p>By using Stycle and any of the content or materials contained on it, or by selecting that you accept or agree to these terms on the registration form, you signify your agreement to:
+
+<ol type="a">
+<li>These Terms</li>
+<li>AllSorts's Privacy Policy, found at http://www.stycle.com.au/privacypolicy</li>
+<li>Other rules and requirements specified in Stycle</li>
+</ol>
+
+<p>You also agree that, in your use of Stycle you will comply with all applicable local, national, and international laws.</p>
+
+<p>If you do not agree to these Terms, you may not use Stycle.</p>
+
+<h2>2. Registration</h2>
+
+<p>You are solely responsible for the activity that occurs on your profile, and you must keep your password safe and secure. You must notify AllSorts immediately of any breach of security on your profile.</p>
+<h2>3. Display Names</h2>
+<p>If you wish to register as member of Stycle, you need to use your first name and family name. 
+AllSorts may refuse to register, or may block, display names it considers inappropriate, including names that contain obscenity, abuse, defamatory content, or copyright infringement.</p>
+<h2>4. Variations to This Site</h2>
+<p>AllSorts may change information on and access to this Site, without notice.</p>
+<p>AllSorts may, in its sole discretion, change these Terms, privacy notice and policies at any time. Changes are publicised at http://stycle.con.au/terms.</p>
+<h2>5. Uploading of items for sale</h2>
+<p>If you wish to submit items for sale on Stycle, you must complete the 'sell an item' form and complete all the necessary fields.</p>
+<p>In uploading content to this Site you represent and warrant to AllSorts that:</p>
+<ol type="a">
+<li>you own all of the intellectual property rights</li>
+<li>the content does not breach any contract or any legal or equitable obligation</li>
+<li>the content is not defamatory, obscene, abusive, invasive of another person's privacy or illegal</li>
+<li>the content is not advertising content other than the item for sale and does not endorse any commercial goods or services or solicit custom for any business enterprise, and is not being licenced for any commercial purpose</li>
+<li>you agree to indemnify and keep indemnified AllSorts and any such licensee of AllSorts against any liability, costs (including legal costs), claims, actions and damages arising from a breach by you or from any representation or warranty made by you being untrue or from use of the content uploaded to Stycle.</li>
+<li>you shall be solely responsible for your own content and the consequences of posting or publishing it</li>
+</ol>
+<h2>6. User Comments</h2>
+AllSorts allows registered users on Stycle to post comments. The opinions expressed in the user comments are not necessarily those of AllSorts. AllSorts is not responsible for, and cannot warrant the accuracy of, any information contained in any user comments.
+<h2>7. Moderation of Uploaded Content and User Comments by AllSorts</h2>
+AllSorts reserves the right to assess the quality of and to moderate, and at its sole discretion, to accept, modify or reject content or user comments. There may be some delay in posting your User-Created content while it is assessed and moderated and AllSorts reserves the right not to post it at all.
+<p>Your content, user comments may be edited, removed or not published if AllSorts considers them to be:</p>
+<ol type="a">
+<li>defamatory or otherwise unlawful, or violating any laws regarding harassment, discrimination, racial vilification, privacy or contempt</li>
+<li>abusive, offensive or obscene</li>
+<li>deliberate provocation of other Stycle users</li>
+<li>compromising your privacy, the privacy of other users or of AllSorts staff, or containing inappropriate personal information</li>
+<li>an infringement of intellectual property rights or copyright</li>
+<li>intentionally false or misleading</li>
+<li>seeking to endorse or promote commercial products, services or activities</li>
+<li>a posting made on behalf of a User whose registration has been terminated or suspended. This may lead to you also having your registration terminated</li>
+</ol>
+If you wish to report a comment, item or user that is breaching the above Terms, report the problem through www.stycle.com/report 
+<h2>8. Removal of Content</h2>
+AllSorts will not knowingly allow on Stycle any content which infringes these Terms.
+Content will be removed from Stycle if it infringes these Terms.  
+<h2>9. Access and Use of Content</h2>
+<p>9.1 You may access and use content, user comments and other content only as permitted under these Terms. </p>
+<p>9.2 Content you upload to the Stycle site will be stored on servers not managed by Stycle as described in the AllSorts Privacy Policy accessible through www.stycle.com.au/privacypolicy</p>
+<h2>10. No Warranty</h2>
+Except as required by non-excludable, statutory warranty, AllSorts makes no warranty or representation:
+<ol type="a">
+<li>as to the accuracy or authenticity of any of Stycle content </li>
+<li>as to the functionality or operation of this Site or the features contained in it</li>
+<li>that Stycle content is free from viruses or defects</li>
+<li>that access to this Site will be uninterrupted</li>
+</ol>
+AllSorts is not responsible for any loss or damage you may incur as a result of your use of Stycle or your use of or reliance on Stycle content.
+<h2>11. Mobile Screen Format</h2>
+Stycle is optimised for mobile screen format. You may not be able to display the information on this Site in any screen other than a mobile screen format. 
+<p>The information contained in this Site is not to be displayed within a frame of another site without AllSorts's written permission.</p>
+<h2>12. Links To and From This Site</h2>
+This Site includes links to other websites. Each linked website has its own terms and conditions of use, and you must ensure that you comply with them. Each link to another website is provided by AllSorts for assistance only. In providing those links, AllSorts does not endorse or make any representation or warranty regarding the products, services or information available via any linked website, or in respect of the owner or operator of that website. 
+<p>AllSorts is not responsible for any loss or damage arising from your use of or access to any linked website.</p>
+<h2>13. Registration Termination Policy</h2>
+AllSorts will block a user's registration if AllSorts, in it's absolute discretion, determines that the user has contravened these Terms or any licence entered into by that User in respect of site content.
+<p>13.2 AllSorts reserves the right to decide whether content is appropriate and complies with the Terms for violations other than copyright infringement, such as, but not limited to, obscene or defamatory material.</p>
+<p>13.3 AllSorts may remove any content and/or terminate a user's access for uploading such material in violation of these Terms at any time, without prior notice and at its sole discretion.</p>
+<h2>14. Privacy</h2>
+AllSorts Privacy Policy is accessbile through www.stycle.com.au/privacypolicy. Please contact the AllSorts Privacy Officer for access, correction and any further privacy information by email at privacy@stycle.com.au or by phoning AllSorts Reception on (02) 0000 0000. 
