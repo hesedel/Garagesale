8765a1aefaa5de69becb243c0c3ea30a6a153d37
AmyScott <amethyst.au@gmail.com>
2014-04-30 23:49:38 +1000

    [#70214008] Privacy Content Added
    Privacy Content Added - HTML added

--- themes/responsive/views/site/pages/policy.php
+++ themes/responsive/views/site/pages/policy.php
@@ -1,1 +1,91 @@
-Policy
+<h1>Privacy</h1>
+
+<p>Please take a few minutes to review the Privacy Policy of Stycle. This policy is effective 30 April 2014.</p>
+
+<p>Stycle’s Privacy Policy describes how Stycle will handle your personal information. If you have any questions please contact the Stycle team.</p>
+
+<h2>Australian Legislation</h2>
+
+<p>Stycle complies with:</p>
+<ul>
+<li>The Privacy Act (1988)</li>
+<li>The 13 Australian Privacy Principles</li>
+<li>The Spam Act (2003)</li>
+</ul>
+
+<h2>Collection</h2>
+<p>You may search and browse Stycle without providing any personal information. However, to list an item, comment, purchase an item through the site or request information, we may collect the following information:</p>
+
+<ul>
+<li>email address</li>
+<li>contact phone number</li>
+<li>study institution</li>
+<li>course area of study</li>
+<li>page views, traffic to and from the site</li>
+<li>other information, including IP addresses and web log information </li>
+</ul>
+
+<p>If you choose to provide Stycle with personal information, you are consenting to Stycle’s collection, use, publication, disclosure and to the transfer and storage of that information on our servers. Stycle servers are located globally.</p>
+
+<h2>Use</h2>
+<p>Stycle uses members personal information to:</p>
+<ul>
+<li>provide you with the services you request</li>
+<li>resolve disputes and troubleshoot problems</li>
+<li>communicate with you and inform you about our services</li>
+<li>encourage safe trading and enforce our policies</li>
+<li>customise your experience</li>
+<li>deliver targeted marketing </li>
+<li>do other things for users as described when Stycle collects the information </li>
+</ul>
+
+<h2>Stycle will not use members personal information to:</h2>
+<ul>
+<li>engage in cross-marketing or link-referral programs with other sites</li>
+<li>employ tracking devices for marketing purposes</li>
+<li>send you unsolicited communications for marketing purposes</li>
+<li>engage in affiliate marketing</li>
+</ul>
+
+<h2>Disclosure</h2>
+<p>Stycle does not sell or rent users personal information to third parties for marketing purposes without users explicit consent.</p>
+
+<p>Stycle may disclose personal information to respond to legal requirements, enforce our policies, respond to claims that a posting or other content violates other’s rights, or protect anyone’s rights, property or safety. </p>
+
+<p>Stycle may also share personal information with:</p>
+<ul>
+<li>corporate affiliates who help detect and prevent potentially illegal acts and provide joint services</li>
+<li>service providers who help with our business operations</li>
+</ul>
+
+<h2>Communication and email tools</h2>
+<p>You agree to receive marketing communications from us unless you tell us that you prefer not to receive such communications. If you don't wish to receive marketing communications from us, simply indicate your preference by following directions provided with the communication.</p>
+
+<p>You may not use our site or communication tools to harvest addresses, send spam or otherwise breach our Terms or Privacy Policy.</p>
+
+<p>Stycle may automatically scan and manually filter email messages sent via our communication tools for malicious activity or prohibited content, but Stycle does not permanently store such messages.</p>
+
+<p>Your message will be emailed to the buyer/seller and not made public. If you use our tools to send content to a friend, Stycle doesn't permanently store your friends addresses or use or disclose them for marketing purposes. </p>
+
+<h2>Access, Modification, and Deletion</h2>
+<p>You can see, modify or erase most of your personal information by reviewing and modifying your comments, items or profile. <p>
+
+<p>You may access your personal information you provided to you us at any time by contacting the Stycle team at privacy@stycle.com.au </p>
+
+<p>Stycle deletes personal information when Stycle no longer need it for the purposes described earlier. Stycle retains personal information as permitted by law to resolve disputes, enforce our policies; and prevent users who break Policy and Terms from coming back.</p>
+
+<h2>Security</h2>
+<p>Stycle works hard to protect your personal information. Tools Stycle uses includes encryption, passwords and physical security to protect your personal information against unauthorized access and disclosure. </p>
+
+<p>While we work hard to protect your personal information, third parties may unlawfully intercept or access transmissions or private messages. Stycle does not promise, and you should not expect, that your personal information or private messages will always remain private.</p> 
+
+<h2>General</h2>
+<p>Stycle may update this policy at any time, with updates taking effect when you next use the site or after 14 days, whichever is sooner. If Stycle is involved in a merger or acquisition, Stycle may share personal information with another company, but this policy will continue to apply. </p>
+
+<p>If you have any questions about Stycle’s Privacy Policy, please contact us at privacy@stycle.com.au </p>
+
+<p>If you do not accept the Privacy Policy, Stycle may not be able to provide you with our services and you may close your account through your dashboard. </p>
+
+
+
+
