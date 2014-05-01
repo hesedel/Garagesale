f1c91673e25a86af97b29d8039b1cbad1cd9511c
Hesedel <hes@pajaroncreative.com>
2014-05-01 17:13:30 +1000

    amended code of utility pages

--- themes/responsive/views/site/pages/about.php
+++ themes/responsive/views/site/pages/about.php
@@ -4,7 +4,8 @@ $this->breadcrumbs=array(
 	'About',
 );
 ?>
-<h1>About</h1>
+
+<h2>About</h2>
 
 <p>Stycle is a student community space to buy and sell second hand items. It is also a space for community shoutouts and for students to find out about events happening locally.</p>
 
@@ -13,5 +14,3 @@ $this->breadcrumbs=array(
 <p>Stycle was founded in May 2014 by a team of University students, Adam, Amy, Hes and Marsha. Their goal was to create a local space for students to make some cash through selling items and also buy items they need for their course, but it evolved as they found out that students needs come in many shapes and forms.</p>
 
 <p>Since it's first sale, Stycle has become a student space to sell anything from course materials, to furniture, to material, to computers, to baby clothes, to last night's fancy dress costume! What will you find on the Stycle site?</p>
-
-<p><tt><?php echo __FILE__; ?></tt>.</p>
