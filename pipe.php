642bfe412e79ed5cc6857a158bfeaa400a689201
Hesedel <hes@pajaroncreative.com>
2014-05-04 01:46:31 +1000

    testing

--- pipe.php
+++ pipe.php
@@ -121,6 +121,7 @@ $headers .= "Reply-To: ". $sender_email . "\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
+/*
 // Tweaked for initial message.
 if ( $item_contact_result->num_rows > 1 )
 	$subject = 'Inquiry for '.$item['title'];
@@ -139,6 +140,7 @@ $html=new CSSToInlineStyles(
 );
 
 $body = $html->convert();
+*/
 
 mail($recipient_email, $subject, $body, $headers);
 
