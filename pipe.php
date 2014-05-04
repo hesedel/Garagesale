0b0b89667fb94f0516831a8e6a10745bf609331d
Hesedel <hes@pajaroncreative.com>
2014-05-04 11:16:24 +1000

    testing

--- pipe.php
+++ pipe.php
@@ -54,7 +54,7 @@ if(substr($decoded[0]['Headers']['content-type:'],0,strlen('text/plain')) == 'te
 } elseif(substr($decoded[0]['Parts'][0]['Headers']['content-type:'],0,strlen('text/plain')) == 'text/plain' && isset($decoded[0]['Parts'][0]['Body'])) {  
 
     $body = nl2br($decoded[0]['Parts'][0]['Body']);
-    file_put_contents(dirname(__FILE__).'/pipe.log', $body);
+    file_put_contents(dirname(__FILE__).'/pipe.log', implode("\n",$decoded));
 
 } elseif(substr($decoded[0]['Parts'][0]['Parts'][0]['Headers']['content-type:'],0,strlen('text/plain')) == 'text/plain' && isset($decoded[0]['Parts'][0]['Parts'][0]['Body'])) {  
 
