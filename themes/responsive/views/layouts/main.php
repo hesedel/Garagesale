a6720f646f876f7b9f7e561a8c7ca5b6864cbcaa
AmyScott <amethyst.au@gmail.com>
2014-05-04 13:26:45 +1000

    [#70575974] Google Analytics Code Applied
    Hi He’s, GA code has been added to the location you had the dummy text
    :) Amy

--- themes/responsive/views/layouts/main.php
+++ themes/responsive/views/layouts/main.php
@@ -65,7 +65,7 @@
 			CHtml::encode(Yii::app()->name)
 		); ?>
 		<h2>Ooops!</h2>
-		<p>Stycle is not yet ready for the table or desktop.</p>
+		<p>Stycle is not yet ready for desktop.</p>
 		<p>Please view this site on mobile phone,<br>or scale your browser window down.</p>
 	</div>
 </div>
@@ -85,17 +85,16 @@ Yii::app()->clientScript->registerPackage('form');
 Yii::app()->clientScript->registerPackage('main');
 ?>
 
-<?php /*
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 <script>
-(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-e.src='//www.google-analytics.com/analytics.js';
-r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-ga('create','UA-XXXXX-X');ga('send','pageview');
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-50631477-1', 'pajaroncreative.com');
+  ga('send', 'pageview');
+
 </script>
-*/ ?>
 
 </body>
 </html>
