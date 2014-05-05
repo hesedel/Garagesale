b3395464a6d6af59aafb4ecc739e362cf652b5ad
AmyScott <amethyst.au@gmail.com>
2014-05-05 22:28:41 +1000

    About Us page update
    Hi He’s, I have updated the About Us content. I had some content
    waiting in google docs…plus I added how the site evolved to become a
    space for students and staff of the University community. Amy

--- themes/responsive/views/site/pages/about.php
+++ themes/responsive/views/site/pages/about.php
@@ -7,10 +7,12 @@ $this->breadcrumbs=array(
 
 <h2>About</h2>
 
-<p>Stycle is a student community space to buy and sell second hand items. It is also a space for community shoutouts and for students to find out about events happening locally.</p>
+<p><b>Stycle is your University space for buying and selling second hand goods via your mobile!</b> Find lots of second hand items for your studies, house and social life. Interesting items can be found under the classification of bizzaro, made by me and freebies. What will you buy and sell on the Stycle site?</p>
 
-<p>The best thing about Stycle is it is free to use! Free to buy and sell! Free to put up a shout out! And you can do all this from your mobile!</p>
+<p><b>Stycle is community driven!</b> With *** active students across all Australian Univerisites, all our members are a part of the student and staff community. Flag your University on your profile to buy and sell items locally. Flag your course area of study to find items that relate to your studies or work. You are also able to find out about and post community classifieds, for example gigs, shout outs as well as jobs for students and staff!</p>
 
-<p>Stycle was founded in May 2014 by a team of University students, Adam, Amy, Hes and Marsha. Their goal was to create a local space for students to make some cash through selling items and also buy items they need for their course, but it evolved as they found out that students needs come in many shapes and forms.</p>
+<p><b>Stycle prides itself on security!</b> Our members provided feedback on what security means to them and we have incorporated that feedback into the Stycle site. Communicate with members before processing a sale, real names and surnames are used and associated with a course area of study, pick up and drop off items at your local uni. If the need arises for you to report a user, comment or item for sale, our team act fast to review your report.</p>
 
-<p>Since it's first sale, Stycle has become a student space to sell anything from course materials, to furniture, to material, to computers, to baby clothes, to last night's fancy dress costume! What will you find on the Stycle site?</p>
+<p><b>The best thing about Stycle is it is free to use!</b> Free to buy and sell! Free to put up a shout out! And you can do all this from your mobile!</p>
+
+<p>Stycle was founded in May 2014 by a team of University students; Adam, Amy, Hes and Marsha. The problem they aimed to solve was the need for students to be able to source affordable products for their studies and everyday living while they were at University. Through user research, they identified that community, security and the need to be able to access the site through mobile devices were important. And thus, Stycle was built. Stycle evolved when University staff members showed interested in posting affordable items for students as they too believed in the importance of 'community'. And Stycle was launched an online campus community for University staff and students to buy and sell.</p>
