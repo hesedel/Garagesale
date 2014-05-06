f27b74360c55ff8b936bcdbf465996ececa4a544
marshamatilda <mmwhite83@gmail.com>
2014-05-06 21:57:24 +1000

    [#70751088]
    Created Homepage Alert - need help with locating CSS.

--- themes/responsive/views/site/index.php
+++ themes/responsive/views/site/index.php
@@ -6,6 +6,19 @@ $this->layout = 'column1';
 
 <div id="site_index">
 
+
+	<div class="alert alert-danger fade in">
+	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+	&times;</button>
+	<div class="h2"><h4>Buy, Sell or give-it-away</h4>
+	<p>Quickly buy or sell to students on your campus. <br> Save Money on your course materials<br>
+	Support your local student community <br><i> And find free stuff!</i></p>
+</div>
+</div>
+</div>
+
+
+
 <div class="h2"><h2>Latest freebies</h2> <?php echo CHtml::link('See all', array('#'), array('class' => 'seeAll')); ?></div>
 
 <?php $this->renderPartial('/item/_index', array(
