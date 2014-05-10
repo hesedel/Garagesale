2d5931d1ced26b972dd90f704d397c8f69ae8d3c
Hesedel <hes@pajaroncreative.com>
2014-05-10 13:28:49 +1000

    [#70210328]

--- protected/migrations/m140427_071102_update_itemCategory_table.php
+++ protected/migrations/m140427_071102_update_itemCategory_table.php
@@ -4,337 +4,287 @@ class m140427_071102_update_itemCategory_table extends CDbMigration
 {
 	public function safeUp()
 	{
+		$this->dropForeignKey('category', 'item');
+		$this->dropForeignKey('parentCategory', 'item_category');
+
+		$this->alterColumn('item', 'category_id', 'smallint UNSIGNED');
+
+		$this->alterColumn('item_category', 'id', 'smallint UNSIGNED');
+		$this->alterColumn('item_category', 'title', 'varchar(32)');
+		$this->alterColumn('item_category', 'parent_id', 'smallint UNSIGNED');
+
+		$this->addForeignKey('category', 'item', 'category_id', 'item_category', 'id', 'SET NULL', 'CASCADE');
+		$this->addForeignKey('parentCategory', 'item_category', 'parent_id', 'item_category', 'id', 'SET NULL', 'CASCADE');
+
 		$this->update('item_category', array(
-			'title'=>'Automotive',
+			'title'=>'Art, Craft, Made by Me',
 		), 'id=1');
 		$this->update('item_category', array(
-			'title'=>'Bikes',
-			'id=2',
-			'parent_id'=>1,
-		));
+			'title'=>'Automotive',
+		), 'id=2');
 		$this->update('item_category', array(
-			'title'=>'Cars',
-			'id=3'
-			'parent_id'=>1,
-		));
+			'title'=>'Books and Stationery',
+		), 'id=3');
 		$this->update('item_category', array(
-			'title'=>'Motorbikes', 
-			'id=4'
-			'parent_id'=>1,
-		));
+			'title'=>'Clothes and Accessories',
+		), 'id=4');
 		$this->insert('item_category', array(
 			'id'=>5,
-			'title'=>'Trikes',
-			'parent_id'=>1,
-		));
-		$this->insert('item_category', array(
-			'id'=>6,
-			'title'=>'Scooters',
-			'parent_id'=>1,
-		));
-		$this->insert('item_category', array(
-			'id'=>9,
 			'title'=>'Electronics',
 		));
 		$this->insert('item_category', array(
-			'id'=>10,
-			'title'=>'Cameras',
-			'parent_id'=>9,
-		));
-		$this->insert('item_category', array(
-			'id'=>11,
-			'title'=>'Computers',
-			'parent_id'=>9,
-		));
-		$this->insert('item_category', array(
-			'id'=>12,
-			'title'=>'Game Consoles',
-			'parent_id'=>9,
-		));
-		$this->insert('item_category', array(
-			'id'=>13,
-			'title'=>'Laptops',
-			'parent_id'=>9,
-		));
-		$this->insert('item_category', array(
-			'id'=>14,
-			'title'=>'Phones',
-			'parent_id'=>9,
-		));
-		$this->insert('item_category', array(
-			'id'=>15,
-			'title'=>'Tablets',
-			'parent_id'=>9,
-		));
-		$this->insert('item_category', array(
-			'id'=>16,
-			'title'=>'Video Cameras',
-			'parent_id'=>9,
-		));
-		$this->insert('item_category', array(
-			'id'=>17,
-			'title'=>'Video Games',
-			'parent_id'=>9,
-		));
-		$this->insert('item_category', array(
-			'id'=>18,
-			'title'=>'Home and Garden',
-		));
-		$this->insert('item_category', array(
-			'id'=>19,
-			'title'=>'Appliances',
-			'parent_id'=>18,
-		));
-		$this->insert('item_category', array(
-			'id'=>20,
-			'title'=>'Garden',
-			'parent_id'=>18,
-		));
-		$this->insert('item_category', array(
-			'id'=>21,
-			'title'=>'Furniture',
-			'parent_id'=>18,
-		));
-		$this->insert('item_category', array(
-			'id'=>22,
-			'title'=>'Kitchenware',
-			'parent_id'=>18,
-		));
-		$this->insert('item_category', array(
-			'id'=>23,
-			'title'=>'Other Home and Garden',
-			'parent_id'=>18,
-		));
-		$this->insert('item_category', array(
-			'id'=>24,
-			'title'=>'Clothes and Accessories',
-		));
-		$this->insert('item_category', array(
-			'id'=>25,
-			'title'=>'Jewellery',
-			'parent_id'=>24,
-		));
-		$this->insert('item_category', array(
-			'id'=>26,
-			'title'=>'Watches',
-			'parent_id'=>24,
-		));
-		$this->insert('item_category', array(
-			'id'=>27,
-			'title'=>'Female Clothes and Shoes',
-			'parent_id'=>24,
-		));
-		$this->insert('item_category', array(
-			'id'=>28,
-			'title'=>'Male Clothes and Shoes',
-			'parent_id'=>24,
-		));
-		$this->insert('item_category', array(
-			'id'=>29,
-			'title'=>'Bags',
-			'parent_id'=>24,
-		));
-		$this->insert('item_category', array(
-			'id'=>30,
-			'title'=>'Accessories',
-			'parent_id'=>24,
-		));
-		$this->insert('item_category', array(
-			'id'=>31,
-			'title'=>'Movies and Music',
-		));
-		$this->insert('item_category', array(
-			'id'=>32,
-			'title'=>'CDs',
-			'parent_id'=>31,
-		));
-		$this->insert('item_category', array(
-			'id'=>33,
-			'title'=>'DVDs',
-			'parent_id'=>31,
-		));
-		$this->insert('item_category', array(
-			'id'=>34,
-			'title'=>'Musical Instruments',
-			'parent_id'=>31,
-		));
-		$this->insert('item_category', array(
-			'id'=>35,
-			'title'=>'Sport and Hobbies',
-		));
-		$this->insert('item_category', array(
-			'id'=>36,
-			'title'=>'Gym and Fitness',
-			'parent_id'=>34,
-		));
-		$this->insert('item_category', array(
-			'id'=>37,
-			'title'=>'Camping and Hiking',
-			'parent_id'=>34,
-		));
-		$this->insert('item_category', array(
-			'id'=>38,
-			'title'=>'Toys',
-			'parent_id'=>34,
-		));
-		$this->insert('item_category', array(
-			'id'=>39,
-			'title'=>'Other Hobbies',
-			'parent_id'=>34,
-		));
-		/* For phase 1, I don't think we need to go to this granular level 
-
-
-		$this->insert('item_category', array(
-			'id'=>40,
-			'title'=>'Skateboards and Rollerblades',
-			'parent_id'=>34,
-		));
-		$this->insert('item_category', array(
-			'id'=>41,
-			'title'=>'Water Sports',
-			'parent_id'=>34,
-		)); */
-		$this->insert('item_category', array(
-			'id'=>42,
+			'id'=>6,
 			'title'=>'Gigs and Entertainment',
 		));
 		$this->insert('item_category', array(
-			'id'=>43,
-			'title'=>'Concerts',
-			'parent_id'=>42,
-		));
-		$this->insert('item_category', array(
-			'id'=>44,
-			'title'=>'Theatre',
-			'parent_id'=>42,
-		));
-		$this->insert('item_category', array(
-			'id'=>45,
-			'title'=>'Other Entertainment',
-			'parent_id'=>42,
-		));
-
-		/* We already have Sports and Hobbies above
-
-		$this->insert('item_category', array(
-			'id'=>46,
-			'title'=>'Sport',
-			'parent_id'=>42,
-		)); */
-		$this->insert('item_category', array(
-			'id'=>47,
-			'title'=>'Art, Craft, Made by Me',
-		));
-		$this->insert('item_category', array(
-			'id'=>48,
-			'title'=>'Art',
-			'parent_id'=>47,
-		));
-		$this->insert('item_category', array(
-			'id'=>49,
-			'title'=>'Made by Me',
-			'parent_id'=>47,
-		));
-		$this->insert('item_category', array(
-			'id'=>50,
-			'title'=>'Antiques, Collectables',
-			'parent_id'=>47,
-		));
-		$this->insert('item_category', array(
-			'id'=>51,
-			'title'=>'Bargains',
-		));
-		$this->insert('item_category', array(
-			'id'=>52,
-			'title'=>'Free Stuff',
-			'parent_id'=>51,
-		));
-		/* Commnuity Noticeboards removed for Phase 1
-
-		$this->insert('item_category', array(
-			'id'=>53,
-			'title'=>'Garage Sale Listing',
-			'parent_id'=>51,
-		));
-		$this->insert('item_category', array(
-			'id'=>54,
-			'title'=>'Community Shout Out',
-		));
-		$this->insert('item_category', array(
-			'id'=>55,
-			'title'=>'Community Noticeboard',
-			'parent_id'=>54,*/
-		));
-		$this->insert('item_category', array(
-			'id'=>56,
+			'id'=>7,
 			'title'=>'Health and Beauty',
 		));
-		/* No select level for Phase 1
 		$this->insert('item_category', array(
-			'id'=>57,
-			'title'=>'Health',
-			'parent_id'=>56,
+			'id'=>8,
+			'title'=>'Home and Garden',
 		));
 		$this->insert('item_category', array(
-			'id'=>58,
-			'title'=>'Beauty',
-			'parent_id'=>56,
-		)); */
+			'id'=>9,
+			'title'=>'Movies and Music',
+		));
+		$this->insert('item_category', array(
+			'id'=>10,
+			'title'=>'Sport and Hobbies',
+		));
 
-		/* Employment Section Removed for Phase 1
+		{
+			$this->insert('item_category', array(
+				'id'=>101,
+				'title'=>'Antiques, Collectables',
+				'parent_id'=>1,
+			));
+			$this->insert('item_category', array(
+				'id'=>102,
+				'title'=>'Art',
+				'parent_id'=>1,
+			));
+			$this->insert('item_category', array(
+				'id'=>103,
+				'title'=>'Made by Me',
+				'parent_id'=>1,
+			));
+		}
 
-		$this->insert('item_category', array(
-			'id'=>59,
-			'title'=>'Employment',
-		));
-		$this->insert('item_category', array(
-			'id'=>60,
-			'title'=>'Internships',
-			'parent_id'=>59,
-		));
-		$this->insert('item_category', array(
-			'id'=>61,
-			'title'=>'Full Time Jobs',
-			'parent_id'=>59,
-		));
-		$this->insert('item_category', array(
-			'id'=>62,
-			'title'=>'Part Time Jobs',
-			'parent_id'=>59,
-		)); */
-		$this->insert('item_category', array(
-			'id'=>63,
-			'title'=>'Books and Stationery',
-		));
-		$this->insert('item_category', array(
-			'id'=>64,
-			'title'=>'Books',
-			'parent_id'=>63,
-		));
-		$this->insert('item_category', array(
-			'id'=>65,
-			'title'=>'Textbooks',
-			'parent_id'=>63,
-		));
-		$this->insert('item_category', array(
-			'id'=>66,
-			'title'=>'Stationery',
-			'parent_id'=>63,
-		));
-		$this->insert('item_category', array(
-			'id'=>67,
-			'title'=>'Wanted',
-		));
-		$this->insert('item_category', array(
-			'id'=>68,
-			'title'=>'Other',
-		));
+		{
+			$this->insert('item_category', array(
+				'id'=>201,
+				'title'=>'Bikes',
+				'parent_id'=>2,
+			));
+			$this->insert('item_category', array(
+				'id'=>202,
+				'title'=>'Cars',
+				'parent_id'=>2,
+			));
+			$this->insert('item_category', array(
+				'id'=>203,
+				'title'=>'Motorbikes',
+				'parent_id'=>2,
+			));
+			$this->insert('item_category', array(
+				'id'=>204,
+				'title'=>'Scooters',
+				'parent_id'=>2,
+			));
+			$this->insert('item_category', array(
+				'id'=>205,
+				'title'=>'Trikes',
+				'parent_id'=>2,
+			));
+		}
+
+		{
+			$this->insert('item_category', array(
+				'id'=>301,
+				'title'=>'Books',
+				'parent_id'=>3,
+			));
+			$this->insert('item_category', array(
+				'id'=>302,
+				'title'=>'Stationery',
+				'parent_id'=>3,
+			));
+			$this->insert('item_category', array(
+				'id'=>303,
+				'title'=>'Textbooks',
+				'parent_id'=>3,
+			));
+		}
+
+		{
+			$this->insert('item_category', array(
+				'id'=>401,
+				'title'=>'Accessories',
+				'parent_id'=>4,
+			));
+			$this->insert('item_category', array(
+				'id'=>402,
+				'title'=>'Bags',
+				'parent_id'=>4,
+			));
+			$this->insert('item_category', array(
+				'id'=>403,
+				'title'=>'Female Clothes and Shoes',
+				'parent_id'=>4,
+			));
+			$this->insert('item_category', array(
+				'id'=>404,
+				'title'=>'Jewellery',
+				'parent_id'=>4,
+			));
+			$this->insert('item_category', array(
+				'id'=>405,
+				'title'=>'Male Clothes and Shoes',
+				'parent_id'=>4,
+			));
+			$this->insert('item_category', array(
+				'id'=>406,
+				'title'=>'Watches',
+				'parent_id'=>4,
+			));
+		}
+
+		{
+			$this->insert('item_category', array(
+				'id'=>501,
+				'title'=>'Cameras',
+				'parent_id'=>5,
+			));
+			$this->insert('item_category', array(
+				'id'=>502,
+				'title'=>'Computers',
+				'parent_id'=>5,
+			));
+			$this->insert('item_category', array(
+				'id'=>503,
+				'title'=>'Game Consoles',
+				'parent_id'=>5,
+			));
+			$this->insert('item_category', array(
+				'id'=>504,
+				'title'=>'Laptops',
+				'parent_id'=>5,
+			));
+			$this->insert('item_category', array(
+				'id'=>505,
+				'title'=>'Phones',
+				'parent_id'=>5,
+			));
+			$this->insert('item_category', array(
+				'id'=>506,
+				'title'=>'Tablets',
+				'parent_id'=>5,
+			));
+			$this->insert('item_category', array(
+				'id'=>507,
+				'title'=>'Video Cameras',
+				'parent_id'=>5,
+			));
+			$this->insert('item_category', array(
+				'id'=>508,
+				'title'=>'Video Games',
+				'parent_id'=>5,
+			));
+		}
+
+		{
+			$this->insert('item_category', array(
+				'id'=>601,
+				'title'=>'Concerts',
+				'parent_id'=>6,
+			));
+			$this->insert('item_category', array(
+				'id'=>602,
+				'title'=>'Theatre',
+				'parent_id'=>6,
+			));
+			$this->insert('item_category', array(
+				'id'=>603,
+				'title'=>'Other Entertainment',
+				'parent_id'=>6,
+			));
+		}
+
+		{
+			$this->insert('item_category', array(
+				'id'=>801,
+				'title'=>'Appliances',
+				'parent_id'=>8,
+			));
+			$this->insert('item_category', array(
+				'id'=>802,
+				'title'=>'Furniture',
+				'parent_id'=>8,
+			));
+			$this->insert('item_category', array(
+				'id'=>803,
+				'title'=>'Garden',
+				'parent_id'=>8,
+			));
+			$this->insert('item_category', array(
+				'id'=>804,
+				'title'=>'Kitchenware',
+				'parent_id'=>8,
+			));
+			$this->insert('item_category', array(
+				'id'=>805,
+				'title'=>'Other Home and Garden',
+				'parent_id'=>8,
+			));
+		}
+
+		{
+			$this->insert('item_category', array(
+				'id'=>901,
+				'title'=>'CDs',
+				'parent_id'=>9,
+			));
+			$this->insert('item_category', array(
+				'id'=>902,
+				'title'=>'DVDs',
+				'parent_id'=>9,
+			));
+			$this->insert('item_category', array(
+				'id'=>903,
+				'title'=>'Musical Instruments',
+				'parent_id'=>9,
+			));
+		}
+
+		{
+			$this->insert('item_category', array(
+				'id'=>1001,
+				'title'=>'Camping and Hiking',
+				'parent_id'=>10,
+			));
+			$this->insert('item_category', array(
+				'id'=>1002,
+				'title'=>'Gym and Fitness',
+				'parent_id'=>10,
+			));
+			$this->insert('item_category', array(
+				'id'=>1003,
+				'title'=>'Toys',
+				'parent_id'=>10,
+			));
+			$this->insert('item_category', array(
+				'id'=>1004,
+				'title'=>'Other Hobbies',
+				'parent_id'=>10,
+			));
+		}
 	}
 
 	public function safeDown()
 	{
-		$this->delete('item_category', 'id between 5 and 68');
+		$this->delete('item_category', 'id between 5 and 2000');
+
 		$this->update('item_category', array(
 			'title'=>'Real Estate',
 		), 'id=4');
@@ -347,5 +297,17 @@ class m140427_071102_update_itemCategory_table extends CDbMigration
 		$this->update('item_category', array(
 			'title'=>'Cars',
 		), 'id=1');
+
+		$this->dropForeignKey('parentCategory', 'item_category');
+		$this->dropForeignKey('category', 'item');
+
+		$this->alterColumn('item_category', 'parent_id', 'tinyint UNSIGNED');
+		$this->alterColumn('item_category', 'title', 'varchar(16)');
+		$this->alterColumn('item_category', 'id', 'tinyint UNSIGNED');
+
+		$this->alterColumn('item', 'category_id', 'tinyint UNSIGNED');
+
+		$this->addForeignKey('parentCategory', 'item_category', 'parent_id', 'item_category', 'id', 'SET NULL', 'CASCADE');
+		$this->addForeignKey('category', 'item', 'category_id', 'item_category', 'id', 'SET NULL', 'CASCADE');
 	}
 }
