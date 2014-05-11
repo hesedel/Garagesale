1392829cdc593bfd92e71c9e7ba02754611740cd
marshamatilda <mmwhite83@gmail.com>
2014-05-11 16:47:28 +1000

    [#70576582]
    Hi Hes,
    
    I have fixed up the primary links but failed to display categories. Lets
    fix on Wednesday.

--- themes/responsive/views/layouts/_table.php
+++ themes/responsive/views/layouts/_table.php
@@ -55,20 +55,36 @@
 				<li><a href="#">All course related items</a></li>
 				<li><a href="#">Viewed by your classmates</a></li>
 				<li><a href="#">Items with the most views</a></li>
-
+				<li><a href="#">Post a wanted ad</a></li>
 
 				<li class="heading">Categories</li>
 
-		
-   		
+		<?php 
+			$categories = Yii::app()->db->createCommand()
+			->select('*')
+			->from('item_category')
+			->order('title')
+			->queryAll();
 
+		// store the categories in a real array
+		$listData = array();
+		foreach($categories as $category)
+			$listData[] = array('id'=>$category['id'], 'title'=>CHtml::encode($category['title']), 'parent_id'=>$category['parent_id']);
 
-				
+		foreach($listData as $i=>$data)
+		{
+			$parent_id = $data['parent_id'];
+			while($parent_id) {
+				$parent = Yii::app()->db->createCommand()
+					->select('*')
+					->from('item_category')
+					->where('id=:parent_id', array(':parent_id'=>$parent_id))
+					->queryRow();
 
-
-				
-			
-				
+		}
+					return $listData;
+	
+		?>
 			</ul>
 		</div>
 
@@ -236,4 +252,5 @@
 
 <?php $this->endWidget(); ?>
 
-</div></div><?php #table-container ?>
+</div></div><?php #table-container 
+?>
