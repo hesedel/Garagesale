a306f04dd513c7cd8825e4dc873a40a938edcbcb
AmyScott <amethyst.au@gmail.com>
2014-05-14 20:58:02 +1000

    [#70574036] - User Profile Updated
    Rearranged HTML to make it more like the wireframe. Still a bit of work
    to do :) but want to send progression to date. Amy

--- themes/responsive/views/admin/user/view.php
+++ themes/responsive/views/admin/user/view.php
@@ -6,25 +6,11 @@ $this->breadcrumbs=array(
 );
 
 $this->layout='column3';
-
-$this->menu=array(
-	array('label'=>'List User', 'url'=>array('index')),
-	array('label'=>'Create User', 'url'=>array('create')),
-	array('label'=>'Update User', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete User', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage User', 'url'=>array('admin')),
-);
 ?>
 
 <?php $this->beginWidget('system.web.widgets.CClipWidget', array('id'=>'sidebar-left')); ?>
 
 <div id="user_view-sidebar-left">
-	<div class="g-sidebar-image"><?php
-		if($model->image)
-			echo CHtml::image('/img/vendor/slir/w263-be8e8e3'.$model->getImage(), $model->id, array('class'=>'image'));
-		else
-			$this->renderPartial('_noImage');
-	?></div>
 
 	<?php /*
 	%table.info-ad
@@ -39,10 +25,10 @@ $this->menu=array(
 			%th Expires on
 			%td =time_local($model->updated, array('offset'=>(60*24*60*60)-(60*60))); // 60 days
 		%tr
-			%th Views
+			%th item_view-sidebar-right
 			%td ...
 	*/ ?>
-</div>
+</div>	
 
 <?php $this->endWidget(); ?>
 
@@ -65,51 +51,49 @@ $this->menu=array(
 <?php $this->endWidget(); ?>
 
 <div class="g-position-relative" id="user_view">
-
-	<h2><?php echo $model->id; ?></h2>
+	<h2> 
+		<!-- Needs to be inline span (or change div to display:inline-block; --><?php
+			if($model->image)
+				echo CHtml::image('/img/vendor/slir/w263-be8e8e3'.$model->getImage(), $model->id, array('class'=>'image'));
+			else
+				$this->renderPartial('_noImage');
+		?>
+		<?php echo $model->id; ?>
+	</h2>
 	<?php $params = array('User'=>$model); ?>
 
-	<div class="g-actions"><?php
+	<div class="g-actions text-right"><?php
 		if(Yii::app()->user->checkAccess('updateSelf',$params) && !Yii::app()->user->checkAccess('super'))
-			echo CHtml::link('Update', array('account'));
+			echo CHtml::link('Update', array('account'), array('class' => 'btn btn-default'));
 		if((Yii::app()->user->checkAccess('admin') && !sizeof(preg_grep('/admin|super/', array_keys(Yii::app()->authManager->getRoles($model->id))))) || Yii::app()->user->checkAccess('super'))
-			echo CHtml::link('Update', array('update', 'id'=>$model->id));
+			echo CHtml::link('Update', array('update', 'id'=>$model->id), array('class' => 'btn btn-default'));
 		if((Yii::app()->user->checkAccess('admin') && !sizeof(preg_grep('/admin|super/', array_keys(Yii::app()->authManager->getRoles($model->id))))) || Yii::app()->user->checkAccess('super'))
-			echo CHtml::link('Delete', '#', array('submit'=>array('delete', 'id'=>$model->id), 'confirm'=>'Are you sure you want to delete this user?'));
+			echo CHtml::link('Delete', '#', array('class' => 'btn btn-default', 'submit'=>array('delete', 'id'=>$model->id), 'confirm'=>'Are you sure you want to delete this user?'));
 	?></div>
-
 	<table class="g-table" cellspacing="1">
-		<tr>
-			<th><?php echo $model->getAttributeLabel('name_first'); ?></th>
-			<td><?php echo $model->name_first; ?></td>
-		</tr>
-		<tr>
-			<th><?php echo $model->getAttributeLabel('name_last'); ?></th>
-			<td><?php echo $model->name_last; ?></td>
-		</tr>
-		<?php if($model->location_id): ?>
-		<tr>
-			<th><?php echo $model->getAttributeLabel('location_id'); ?></th>
-			<td><?php echo $model->location->name; ?></td>
-		</tr>
-		<?php endif; ?>
-		<?php if($model->id === Yii::app()->user->id || Yii::app()->user->checkAccess('admin') || Yii::app()->user->checkAccess('super')): ?>
+	<tr>
+  		<th>Name</th>
+  		<td><?php echo $model->name_first; ?></td>
+  	</tr>
+  	<tr>
+  		<th>University</th>
+  		<td>Input</td>
+  	</tr>
+  	<tr>
+  		<th>Joined</th>
+  		<td><?php echo time_local($model->created); ?></td>
+	</tr>
+	<?php if($model->id === Yii::app()->user->id || Yii::app()->user->checkAccess('admin') || Yii::app()->user->checkAccess('super')): ?>
+
+
 		<tr>
 			<th><?php echo $model->getAttributeLabel('email'); ?></th>
 			<td><?php echo $model->email; ?></td>
-		<?php endif; ?>
 		</tr>
 		<tr>
 			<th><?php echo $model->getAttributeLabel('phone'); ?></th>
 			<td><?php echo $model->phone; ?></td>
 		</tr>
-		<tr>
-			<th><?php echo $model->getAttributeLabel('created'); ?> On</th>
-			<td><?php echo time_local($model->created); ?></td>
-		</tr>
-		<tr>
-			<th>Last Logged-In On</th>
-			<td>...</td>
-		</tr>
-	</table>
+		</table>
+	<?php endif; ?>
 </div>
