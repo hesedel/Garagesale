230b3bdc724c2008b72dbcb722f3aaee1eb4c4d2
Hesedel <hes@pajaroncreative.com>
2014-05-27 14:13:38 +1000

    test

---
+++ themes/responsive/views/admin/user/password-change.php
@@ -0,0 +1,63 @@
+<?php
+$this->pageTitle=Yii::app()->name . ' - Change Password';
+?>
+
+<div id="user_changePassword">
+
+	<div class="form">
+
+		<?php
+		$form=$this->beginWidget('CActiveForm', array(
+			'id'=>'user-passwordChange-form',
+			'enableClientValidation'=>true,
+			'focus'=>array($model,'password'),
+		));
+		?>
+
+		<table class="form" summary="Password Change">
+			<caption class="hide">Password Change</cation>
+
+			<tfoot>
+
+				<tr>
+					<th>\\&#160;</th>
+					<td>
+						<?php echo CHtml::linkButton('Change Password', array('class'=>'submit g-button orange')); ?>
+						<?php echo CHtml::submitButton('Change Password', array('class'=>'submit g-button orange')); ?>
+					</td>
+				</tr>
+
+			</tfoot>
+			<tbody>
+
+				<tr>
+					<th><?php echo $form->labelEx($model,'password'); ?></th>
+					<td>
+						<div class="input-text">
+							<?php echo $form->passwordField($model,'password'); ?>
+							<span class="placeholder">Create a new password</span>
+						</div>
+						<?php echo $form->error($model,'password'); ?>
+				</tr>
+
+				<tr>
+					<th><?php echo $form->labelEx($model,'password_repeat'); ?>
+					<td>
+						<div class="input-text">
+							<?php echo $form->passwordField($model,'password_repeat'); ?>
+							<span class="placeholder">Re-enter the password you created</span>
+						</div>
+						<?php echo $form->error($model,'password_repeat'); ?>
+					</td>
+				</tr>
+
+			</tbody>
+		</table>
+
+		<?php
+		$this->endWidget();
+		?>
+
+	</form>
+
+</div>
