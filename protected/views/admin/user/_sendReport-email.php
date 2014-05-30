6575b4f279ca74bd621bc25fb9f4ea8d108a966a
Hesedel <hes@pajaroncreative.com>
2014-05-31 00:46:24 +1000

    report

--- protected/views/admin/user/_sendReport-email.php
+++ protected/views/admin/user/_sendReport-email.php
@@ -4,13 +4,11 @@ User reported: <?php echo Item::model()->findByPk($_GET['item_id'])->user->name_
 Report type:
 
 <?php foreach($model->reportType as $report): ?>
-
 <?php echo $report; ?>
 
 <?php endforeach; ?>
 
 Report descrption:
-
 <?php echo $model->reportDescription; ?>
 
 
