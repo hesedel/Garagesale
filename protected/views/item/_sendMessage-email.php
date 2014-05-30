af70b08a3778eb763c1a3c4ede9878984e271a83
Hesedel <hes@pajaroncreative.com>
2014-05-31 01:07:52 +1000

    test

--- protected/views/item/_sendMessage-email.php
+++ protected/views/item/_sendMessage-email.php
@@ -4,9 +4,7 @@ Dear <?php echo $name_poster; ?>,
 You've received this email because you've just recently received a message
 from <?php echo $name_replier; ?>.
 
-<div id="message">
-<?php echo $message; ?>
-</div>
+<div id="message"><?php echo $message; ?></div>
 
 Link to item: <?php echo $link; ?>
 
