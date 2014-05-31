ef18362c83bc1607a49e3beb34e2a850cd526d8b
AmyScott <amethyst.au@gmail.com>
2014-06-01 00:08:52 +1000

    [#72178808] [#72178736] Utility and About updates
    All utility pages have been checked and updated :) Accordions applied
    to the about page :)

--- themes/responsive/views/site/pages/terms.php
+++ themes/responsive/views/site/pages/terms.php
@@ -3,121 +3,121 @@ $this->breadcrumbs=array(     'Terms', ); ?>
 
 <h2>Terms</h2>
 
-<p>These Terms apply to all users of Stycle, including users who are not registered users. Please read the Terms carefully before using Stycle.</p>
+<p>These Terms apply to all users of stycle, including users who are not registered users. Please read the Terms carefully before using stycle.</p>
 
-<h3>1. Access to Stycle</h3>
+<h3>1. Access to stycle</h3>
 
-<p>By using Stycle and any of the content or materials contained on it, or by selecting that you accept or agree to these terms on the registration form, you signify your agreement to:</p>
+<p>By using stycle and any of the content or materials contained on it, or by selecting that you accept or agree to these terms on the registration form, you signify your agreement to:</p>
 
 <ol type="a">
 	<li>These Terms</li>
-	<li>AllSorts's <a href="http://sta-stycle.pajaroncreative.com/site/page/?view=policy">Privacy Policy</a></li>
-	<li>Other rules and requirements specified in Stycle</li>
+	<li><a href="http://sta-stycle.pajaroncreative.com/site/page/?view=policy">Privacy Policy</a></li>
+	<li>Other rules and requirements specified in stycle</li>
 </ol>
 
-<p>You also agree that, in your use of Stycle you will comply with all applicable local, national, and international laws.</p>
+<p>You also agree that, in your use of stycle you will comply with all applicable local, national, and international laws.</p>
 
-<p>If you do not agree to these Terms, you may not use Stycle.</p>
+<p>If you do not agree to these Terms, you may not use stycle.</p>
 
 <h3>2. Registration</h3>
 
-<p>You are solely responsible for the activity that occurs on your profile, and you must keep your password safe and secure. You must notify AllSorts immediately of any breach of security on your profile.</p>
+<p>You are solely responsible for the activity that occurs on your profile, and you must keep your password safe and secure. You must notify stycle immediately of any breach of security on your profile.</p>
 
-<p>Stycle is only available to University staff and students. On registration a University email address required.</p>
+<p>stycle is only available to Australian University staff and students. On registration an Australian University email address required.</p>
 
-<h3>3. Display Names</h3>
+<h3>3. Display names</h3>
 
-<p>If you wish to register as member of Stycle, you need to use your first name and family name. 
-AllSorts may refuse to register, or may block, display names it considers inappropriate, including names that contain obscenity, abuse, defamatory content, or copyright infringement.</p>
+<p>If you wish to register as member of stycle, you need to use your first name and family name. 
+stycle may refuse to register, or may block, display names it considers inappropriate, including names that contain obscenity, abuse, defamatory content, or copyright infringement.</p>
 
-<h3>4. Variations to This Site</h3>
+<h3>4. Variations to this site</h3>
 
-<p>AllSorts may change information on and access to this Site, without notice.</p>
+<p>stycle may change information on and access to this site, without notice.</p>
 
-<p>AllSorts may, in its sole discretion, change these Terms, privacy notice and policies at any time. Changes are publicised at <a href="http://sta-stycle.pajaroncreative.com/site/page/?view=terms">Stycle Terms</a>.</p>
+<p>stycle may, in its sole discretion, change these Terms, privacy notice and policies at any time. Changes are publicised at <a href="http://sta-stycle.pajaroncreative.com/site/page/?view=terms">stycle Terms</a>.</p>
 
 <h3>5. Uploading of items for sale</h3>
 
-<p>If you wish to submit items for sale on Stycle, you must complete the 'sell an item' form and complete all the necessary fields.</p>
+<p>If you wish to submit items for sale on stycle, you must complete the 'sell an item' form and complete all the necessary fields.</p>
 
-<p>In uploading content to this Site you represent and warrant to AllSorts that:</p>
+<p>In uploading content to this site you represent and warrant to stycle that:</p>
 
 <ol type="a">
 	<li>you own the item/s you list</li>
 	<li>the content does not breach any contract or any legal or equitable obligation</li>
 	<li>the content is not defamatory, obscene, abusive, invasive of another person's privacy or illegal</li>
 	<li>the content is not advertising content other than the item for sale and does not endorse any commercial goods or services or solicit custom for any business enterprise, and is not being licenced for any commercial purpose</li>
-	<li>you agree to indemnify and keep indemnified AllSorts and any such licensee of AllSorts against any liability, costs (including legal costs), claims, actions and damages arising from a breach by you or from any representation or warranty made by you being untrue or from use of the content uploaded to Stycle</li>
+	<li>you agree to indemnify and keep indemnified stycle and any such licensee of stycle against any liability, costs (including legal costs), claims, actions and damages arising from a breach by you or from any representation or warranty made by you being untrue or from use of the content uploaded to stycle</li>
 	<li>you shall be solely responsible for your own content and the consequences of posting or publishing it</li>
 </ol>
 
-<h3>6. User Messages</h3>
+<h3>6. Emails</h3>
 
-<p>AllSorts allows registered users on Stycle to message other users via a secure email service. The opinions expressed in the user messages are not necessarily those of AllSorts. AllSorts is not responsible for, and cannot warrant the accuracy of, any information contained in any user messages.</p>
+<p>stycle allows registered users on stycle to email other users via a secure email service. The opinions expressed in the user messages are not necessarily those of stycle. stycle is not responsible for, and cannot warrant the accuracy of, any information contained in any user emails.</p>
 
-<h3>7. Moderation of Uploaded Content and User Messages</h3>
+<h3>7. Moderation of uploaded content</h3>
 
-<p>AllSorts reserves the right to assess the quality of and to moderate, and at its sole discretion, to accept, modify or reject content. There may be some delay in posting your User-Created content while it is assessed and moderated and AllSorts reserves the right not to post it at all.</p>
+<p>stycle reserves the right to assess the quality of and to moderate, and at its sole discretion, to accept, modify or reject content. There may be some delay in posting your User-Created content while it is assessed and moderated and stycle reserves the right not to post it at all.</p>
 
-<p>Your content, may be edited, removed or not published if AllSorts considers them to be:</p>
+<p>Your content, may be edited, removed or not published if stycle considers them to be:</p>
 
 <ol type="a">
 	<li>defamatory or otherwise unlawful, or violating any laws regarding harassment, discrimination, racial vilification, privacy or contempt</li>
 	<li>abusive, offensive or obscene</li>
-	<li>deliberate provocation of other Stycle users</li>
-	<li>compromising your privacy, the privacy of other users or of AllSorts staff, or containing inappropriate personal information</li>
+	<li>deliberate provocation of other stycle users</li>
+	<li>compromising your privacy, the privacy of other users or of stycle staff, or containing inappropriate personal information</li>
 	<li>an infringement of intellectual property rights or copyright</li>
 	<li>intentionally false or misleading</li>
 	<li>seeking to endorse or promote commercial products, services or activities</li>
 	<li>a posting made on behalf of a User whose registration has been terminated or suspended. This may lead to you also having your registration terminated</li>
 </ol>
 
-<p>If you wish to report an item, message or user who breaches the above Terms, report the problem through http://sta-stycle.pajaroncreative.com/site/report</p>
+<p>If you wish to report an item, message or user who breaches the above Terms, report user or item.</p>
 
-<h3>8. Removal of Content</h3>
+<h3>8. Removal of content</h3>
 
-<p>AllSorts will not knowingly allow on Stycle any content which infringes these Terms.
-Content will be removed from Stycle if it infringes these Terms.</p>
+<p>stycle will not knowingly allow on stycle any content which infringes these Terms.
+Content will be removed from stycle if it infringes these Terms.</p>
 
-<h3>9. Access and Use of Content</h3>
+<h3>9. Access and use of content</h3>
 
-<p>9.1 You may access and use content, user comments and other content only as permitted under these Terms.</p>
+<p>You may access content, emails and other content only as permitted under these Terms.</p>
 
-<p>9.2 Content you upload to the Stycle site will be stored on servers not managed by Stycle as described in the AllSorts <a href="http://sta-stycle.pajaroncreative.com/site/page/?view=policy">Privacy Policy</a></p>
+<p>Content you upload to the stycle site will be stored on servers not managed by stycle as described in the stycle <a href="http://sta-stycle.pajaroncreative.com/site/page/?view=policy">Privacy Policy</a></p>
 
-<h3>10. No Warranty</h3>
+<h3>10. No warranty</h3>
 
-<p>Except as required by non-excludable, statutory warranty, AllSorts makes no warranty or representation:</p>
+<p>Except as required by non-excludable, statutory warranty, stycle makes no warranty or representation:</p>
 
 <ol type="a">
-	<li>as to the accuracy or authenticity of any of Stycle content</li>
-	<li>as to the functionality or operation of this Site or the features contained in it</li>
-	<li>that Stycle content is free from viruses or defects</li>
-	<li>that access to this Site will be uninterrupted</li>
+	<li>as to the accuracy or authenticity of any of stycle content</li>
+	<li>as to the functionality or operation of this site or the features contained in it</li>
+	<li>that stycle content is free from viruses or defects</li>
+	<li>that access to this site will be uninterrupted</li>
 </ol>
 
-<p>AllSorts is not responsible for any loss or damage you may incur as a result of your use of Stycle or your use of or reliance on Stycle content.</p>
+<p>stycle is not responsible for any loss or damage you may incur as a result of your use of stycle or your use of or reliance on stycle content.</p>
 
-<h3>11. Mobile Screen Format</h3>
+<h3>11. Mobile screen format</h3>
 
-<p>Stycle is optimised for mobile screen format. You may not be able to display the information on this Site in any screen other than a mobile screen format.</p>
+<p>stycle is optimised for mobile screen format. You may not be able to display the information on this site in any screen other than a mobile screen format.</p>
 
-<p>The information contained in this Site is not to be displayed within a frame of another site without AllSorts's written permission.</p>
+<p>The information contained in this site is not to be displayed within a frame of another site without stycle's written permission.</p>
 
-<h3>12. Links To and From This Site</h3>
+<h3>12. Links to and from stycle</h3>
 
-<p>This Site includes links to other websites. Each linked website has its own terms and conditions of use, and you must ensure that you comply with them. Each link to another website is provided by AllSorts for assistance only. In providing those links, AllSorts does not endorse or make any representation or warranty regarding the products, services or information available via any linked website, or in respect of the owner or operator of that website.</p>
+<p>This site includes links to other websites. Each linked website has its own terms and conditions of use, and you must ensure that you comply with them. Each link to another website is provided by stycle for assistance only. In providing those links, stycle does not endorse or make any representation or warranty regarding the products, services or information available via any linked website, or in respect of the owner or operator of that website.</p>
 
-<p>AllSorts is not responsible for any loss or damage arising from your use of or access to any linked website.</p>
+<p>stycle is not responsible for any loss or damage arising from your use of or access to any linked website.</p>
 
-<h3>13. Registration Termination Policy</h3>
+<h3>13. Registration termination policy</h3>
 
-<p>AllSorts will block a user's registration if AllSorts, in it's absolute discretion, determines that the user has contravened these Terms or any licence entered into by that User in respect of site content.</p>
+<p>stycle will block a user's registration if stycle, in it's absolute discretion, determines that the user has contravened these Terms or any licence entered into by that User in respect of site content.</p>
 
-<p>13.2 AllSorts reserves the right to decide whether content is appropriate and complies with the Terms for violations other than copyright infringement, such as, but not limited to, obscene or defamatory material.</p>
+<p>stycle reserves the right to decide whether content is appropriate and complies with the Terms for violations other than copyright infringement, such as, but not limited to, obscene or defamatory material.</p>
 
-<p>13.3 AllSorts may remove any content and/or terminate a user's access for uploading such material in violation of these Terms at any time, without prior notice and at its sole discretion.</p>
+<p>stycle may remove any content and/or terminate a user's access for uploading such material in violation of these Terms at any time, without prior notice and at its sole discretion.</p>
 
 <h3>14. Privacy</h3>
 
-<p>Access AllSorts <a href="http://sta-stycle.pajaroncreative.com/site/page/?view=policy">Privacy Policy</a>. Contact the AllSorts Privacy Officer if you have any questions about the Privacy Policy.</p>
+<p>Access stycle's <a href="http://sta-stycle.pajaroncreative.com/site/page/?view=policy">Privacy Policy</a>.</p>
